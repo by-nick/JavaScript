7a460be9d112af1c1b4892ac6295706fa7fe8d49
Nick <laribrito78@gmail.com>
2025-01-09 17:46:47 -0300

    Condição Simples
    sem o else

---
+++ aula11/ex008.js
@@ -0,0 +1,5 @@
+var vel = 78
+if (vel > 60){ //condição simples, pois não tem o "senao"
+    console.log(`Sua velocidade é de ${vel}, você está acima da velocidade permitida. MULTADO!`)
+}
+console.log(`Sua velocidade é de ${vel},sua velocidade esta abaixo do permitido, dirija com cudado!`)
