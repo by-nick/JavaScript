e6e06ba4557d2f48de8ace537308e18febae637b
Nick <laribrito78@gmail.com>
2025-01-09 17:47:07 -0300

    Condição composta
    com o else

---
+++ aula11/ex009.js
@@ -0,0 +1,8 @@
+var p = 'França'
+console.log(`Você mora em ${p}`)
+
+if (p=='Brasil'){//condição composta, pois contém o "senao"
+    console.log(`Vivendo em ${p} você é Brasileiro`)
+}else{
+    console.log(`Vivendo em ${p} você é estrangeiro`)
+}
